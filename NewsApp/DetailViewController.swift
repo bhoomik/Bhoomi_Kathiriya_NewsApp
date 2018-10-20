86ee16d687ce1914b1f9282e9b6698033a826267
Bhavik Kathiriya <bhoomi@bhoomis-macbook-air.local>
2018-10-20 13:15:56 +0530

    Initial Commit

---
+++ NewsApp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  NewsApp
+//
+//  Created by Bhoomi Kathiriya on 20/10/18.
+//  Copyright © 2018 Bhoomi Kathiriya. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
