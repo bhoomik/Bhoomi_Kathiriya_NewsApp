1280a53ea3f87922d8501ee07ddd278cec4a399f
Bhavik Kathiriya <bhoomi@bhoomis-macbook-air.local>
2018-10-21 18:00:41 +0530

    Added test case

--- NewsAppTests/NewsAppTests.swift
+++ NewsAppTests/NewsAppTests.swift
@@ -11,6 +11,10 @@ import XCTest
 
 class NewsAppTests: XCTestCase {
 
+    private var newsService : NewsService = NewsService()
+
+    let strURL = String(format: "%@%@",kBaseURL,kAPIKey)
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -23,6 +27,23 @@ class NewsAppTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    func testParsing() {
+        
+        let expectation2 = expectation(description: "Parsing Succeeds")
+        
+        
+        newsService.createRequest(qMes: "", strURL: strURL, method: "GET", completionBlock: { (arg0) in
+            expectation2.fulfill()
+            let (output) = arg0
+            print("test case output",output)
+        }, andFailureBlock: { (failure) in
+            XCTFail()
+        })
+        waitForExpectations(timeout: 600.0) { (_) -> Void in
+        }
+        
+    }
+    
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
